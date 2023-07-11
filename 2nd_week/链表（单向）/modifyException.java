c17234897f0653f283cea30648b88766fa6eb056
Ytangxk <101381642+Ytangxk@users.noreply.github.com>
2023-07-11 10:38:40 +0800

    Delete 2nd_week directory

--- 2nd_week/链表（单向）/modifyException.java
+++
@@ -1,8 +0,0 @@
-package cn.Ytangxk.javase.collection.List;
-
-public class modifyException extends Exception{
-    @Override
-    public String getMessage() {
-        return "修改失败，需要修改的元素不存在！";
-    }
-}
--- 2nd_week/链表（单向）/removeException.java
+++
@@ -1,9 +0,0 @@
-package cn.Ytangxk.javase.collection.List;
-
-public class removeException extends Exception{
-
-    @Override
-    public String getMessage() {
-        return "删除不存在元素！";
-    }
-}
